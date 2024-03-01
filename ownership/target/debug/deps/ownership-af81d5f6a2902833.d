f0047d5bec283c5c6b1b9243e9b5d2fc84e7b10b
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-03-01 19:02:47 +0500

    Ownership concept in Rust

---
+++ ownership/target/debug/deps/ownership-af81d5f6a2902833.d
@@ -0,0 +1,5 @@
+/root/Rust/ownership/ownership/target/debug/deps/libownership-af81d5f6a2902833.rmeta: src/main.rs
+
+/root/Rust/ownership/ownership/target/debug/deps/ownership-af81d5f6a2902833.d: src/main.rs
+
+src/main.rs:
