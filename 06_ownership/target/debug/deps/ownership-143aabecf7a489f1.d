eea3b001bd2da02424374e7f96b1d32cc1ed0277
Hammad Ali Khan <151325222+HammadAliKhanweb3@users.noreply.github.com>
2024-03-02 00:41:27 +0500

    Add ownership concept in Rust

---
+++ 06_ownership/target/debug/deps/ownership-143aabecf7a489f1.d
@@ -0,0 +1,5 @@
+/root/Rust/ownership/ownership/target/debug/deps/libownership-143aabecf7a489f1.rmeta: src/main.rs
+
+/root/Rust/ownership/ownership/target/debug/deps/ownership-143aabecf7a489f1.d: src/main.rs
+
+src/main.rs:
---
+++ 06_ownership/target/debug/deps/ownership-b518c9a450793be7.d
@@ -0,0 +1,5 @@
+/root/Rust/ownership/ownership/target/debug/deps/ownership-b518c9a450793be7: src/main.rs
+
+/root/Rust/ownership/ownership/target/debug/deps/ownership-b518c9a450793be7.d: src/main.rs
+
+src/main.rs:
